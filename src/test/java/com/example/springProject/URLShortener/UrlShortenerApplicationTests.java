453da6eeee0198b281cddaccb3ad7be469b444c3
Nayan Gupta <kanumgupta32@gmail.com>
2024-04-28 20:58:21 +0530

    Initial Commit

---
+++ src/test/java/com/example/springProject/URLShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springProject.URLShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
