eadfeb222944ba5a57dd9c042b575edb5f8dc16b
Nayan Gupta <kanumgupta32@gmail.com>
2024-05-04 21:49:00 +0530

    commented out some function

---
+++ src/main/java/com/example/springProject/URLShortener/URLShortenerController.java
@@ -0,0 +1,18 @@
+package com.example.springProject.URLShortener;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class URLShortenerController {
+//    @PostMapping(path = "/submit")
+//    public ResponseEntity<url> postURL(@RequestBody url){
+//        //Check if the URL present in the Database.
+//        //If not present in the Database then Update the Database.
+//
+//    }
+}
